d0f2483874b527bf8814b8244355e0c602957f1f
amitm144 <87026850+amitm144@users.noreply.github.com>
2024-09-16 16:30:22 +0300

    redirect uri fix

--- config/passportConfig.js
+++ config/passportConfig.js
@@ -5,7 +5,9 @@ module.exports = function(passport) {
   passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "/auth/google/callback"
+    callbackURL: process.env.NODE_ENV === 'production'
+    ? "https://iroommieserver.onrender.com/auth/google/callback"
+    : "http://localhost:3001/auth/google/callback"
   },
   async (accessToken, refreshToken, profile, done) => {
     // The user data will be handled in the googleAuthCallback in authController
--- server.js
+++ server.js
@@ -21,6 +21,7 @@ mongoose.connect(process.env.MONGODB_URI);
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", () => {
+  console.log("Connected to MongoDB");
 })
 
 // Session configuration
