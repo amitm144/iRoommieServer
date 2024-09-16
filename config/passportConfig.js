c05672ed608fac6b4dd33ae9e6440f85e49cf776
amitm144 <87026850+amitm144@users.noreply.github.com>
2024-09-16 16:39:48 +0300

    FIX REDIRECT

--- config/passportConfig.js
+++ config/passportConfig.js
@@ -6,8 +6,8 @@ module.exports = function(passport) {
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     callbackURL: process.env.NODE_ENV === 'production'
-    ? "https://iroommieserver.onrender.com/auth/google/callback"
-    : "http://localhost:3001/auth/google/callback"
+    ? CONFIG_REDIRECT_URL_PROD
+    : CONFIG_REDIRECT_URL_DEV 
   },
   async (accessToken, refreshToken, profile, done) => {
     // The user data will be handled in the googleAuthCallback in authController
